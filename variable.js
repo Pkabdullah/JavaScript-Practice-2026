a162e2d6609404c705211be7ae07b4a894f75de0
Muhammad Abdullah <abdullah.sheikhaps@gmail.com>
2026-02-04 11:36:13 +0500

    JavaScript-Practice

---
+++ variable.js
@@ -0,0 +1,18 @@
+console.log("Variables")
+var a ="abdullah";
+var b  =45;
+let c = "karachi"
+c="Pakistan";
+console.log(a);
+
+{
+    let b= "testing";
+    console.log(b)
+}
+console.log(b);
+
+console.log(c);
+
+// Var is not a block scope element
+//let is a block scoped element
+
