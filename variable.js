7882f87f8f2721e3b291694907329410dfabdde4
Muhammad Abdullah <abdullah.sheikhaps@gmail.com>
2026-02-04 11:45:48 +0500

    updated file

--- variable.js
+++ variable.js
@@ -1,17 +1,19 @@
 console.log("Variables")
-var a ="abdullah";
-var b  =45;
+var a = "abdullah";
+var b = 45;
 let c = "karachi"
-c="Pakistan";
+c = "Pakistan";
+const author = "Muhammad Abdullah";
+author = "testing const variable"; //throw an error because const variable cannot be changed
 console.log(a);
-
 {
-    let b= "testing";
+    let b = "testing";
     console.log(b)
 }
 console.log(b);
-
 console.log(c);
+console.log(author);
+
 
 // Var is not a block scope element
 //let is a block scoped element
